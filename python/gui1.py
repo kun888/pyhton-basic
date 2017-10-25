2890fdbb5cd6af2537906ceee86c4398e3ece8d8
unknown <kunalchawla2016@gmail.com>
2017-10-25 15:22:38 +0530

    graphical user interface1

---
+++ python/gui1.py
@@ -0,0 +1,11 @@
+import tkinter
+import tkMessageBox
+top = Tkinter.Tk()
+
+def helloCallBack():
+   tkMessageBox.showinfo( "Hello Python", "Hello World")
+
+B = Tkinter.Button(top, text ="Hello", command = helloCallBack)
+
+B.pack()
+top.mainloop()
